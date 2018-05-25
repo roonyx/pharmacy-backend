fca062c92fb4300a61883d1d1117078d7da5bb50
Eugene Yak <geneayak@gmail.com>
2018-05-25 20:36:19 +0800

    add order controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+  def create
+    pdf = Prawn::Document.new
+
+    pdf.font_size(25) { pdf.text "Order #{Time.now}" }
+
+    params[:order].each do |ingredient|
+      pdf.text ingredient['name']
+      pdf.text ingredient['percentage'].to_s + '%'
+      pdf.text ingredient['description']
+      pdf.move_down 20
+    end
+
+    send_data pdf.render,
+              filename: "order",
+              type: 'application/pdf',
+              disposition: 'inline'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :ingredients, only: :index
   resources :formulations, only: :index
+  resource :order, only: :create
 end
