5b4bf4cad3c984459633440fd99c7d02715acfbe
Eugene Yak <geneayak@gmail.com>
2018-05-28 18:26:58 +0800

    refactor formulation index

--- app/controllers/formulations_controller.rb
+++ app/controllers/formulations_controller.rb
@@ -1,16 +1,5 @@
 class FormulationsController < ApplicationController
-  # def index
-  #   res = FormulationIngredient.includes(:ingredient, :formulation)
-  #                     .group_by { |e| e.formulation }
-  #
-  #   res.keys.map! { |e| e.name }
-  #
-  #   render json: res
-  # end
-
   def index
-    formulations = Formulation.all.includes(:ingredients)
-
-    render json: formulations
+    render json: Formulation.all.includes(:ingredients)
   end
 end
